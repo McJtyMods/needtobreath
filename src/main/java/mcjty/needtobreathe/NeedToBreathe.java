180db9a362a42880d8497b8a050cd9ef22c0e3f9
McJty <mcjty1@gmail.com>
2018-06-04 17:10:09 +0200

    McJtyLib 3.0.0-alpha -> 3.0.0

--- src/main/java/mcjty/needtobreathe/NeedToBreathe.java
+++ src/main/java/mcjty/needtobreathe/NeedToBreathe.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.Logger;
         acceptedMinecraftVersions = "[1.12,1.13)")
 public class NeedToBreathe implements ModBase {
     public static final String MODID = "needtobreathe";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0-alpha";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0";
     public static final String VERSION = "0.2.0-alpha";
     public static final String MIN_FORGE_VER = "14.22.0.2464";
 
